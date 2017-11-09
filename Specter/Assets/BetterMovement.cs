165f707321a7370118d64bab43562b987f734be2
SaintRickolas <richardmicahelpereira@gmail.com>
2017-11-09 18:39:17 +1100

    Initial project commit

---
+++ Specter/Assets/BetterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterMovement : MonoBehaviour 
+{
+
+
+	public float moveSpeed = 10f;
+	public float turnSpeed = 100f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 translation = moveSpeed * Time.deltaTime * (Input.GetAxis ("Horizontal") * Vector3.right + Input.GetAxis ("Vertical") * Vector3.forward);
+		transform.Translate (translation, Space.World);
+
+//		transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.LookRotation (translation), turnSpeed * Time.deltaTime * translation.magnitude);
+		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (translation), turnSpeed * Time.deltaTime * translation.magnitude);
+	}
+}
---
+++ Specter/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float Speed = 7f; 
+	public float smoothMoveTime = .1f;
+	public float turnSpeed = 8f;
+
+
+	float smoothInputMagnitude;
+	float smoothMoveVelocity;
+	float angle; 
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 inputDirection = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized;
+		float inputMagnitude = inputDirection.magnitude;
+		smoothInputMagnitude = Mathf.SmoothDamp (smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+		float targetAngle = Mathf.Atan2 (inputDirection.x, inputDirection.z) * Mathf.Rad2Deg; 
+		transform.eulerAngles = Vector3.up * angle;
+		angle = Mathf.LerpAngle (angle, targetAngle, Time.deltaTime * turnSpeed *inputMagnitude);
+
+
+		transform.Translate (transform.forward * Speed * Time.deltaTime * smoothInputMagnitude, Space.World);
+		
+	}
+}
